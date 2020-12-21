cdece238cb107fa5a4957cf90231cef60f018d65
Pham The Giau <58292695+jpeace08@users.noreply.github.com>
2020-12-21 21:45:25 +0700

    scfs

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
+/*goi thư viện để cấu hình server*/
 var express = require('express');
 const { disconnect, exit } = require("process");
 var app = express();
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-app.set('views', './views');
 
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 var mysql = require('mysql');
 
 var bodyParser =  require('body-parser');
 var logger =  require ('morgan');
 
+/*Goi thưu viện socket io*/
+var io = require('socket.io')(server);
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
@@ -21,6 +23,8 @@ server.listen(3000);
 
 let stateDoor;
 let isEnable;
+
+// kết thúc cấu hình server
 
 // connect database:
 var con = mysql.createConnection({
@@ -34,6 +38,7 @@ var con = mysql.createConnection({
     if (err) throw err;
     console.log("Connected!");
   });
+
 
 const sendShut = () => {
     let date = new Date;
@@ -63,7 +68,7 @@ setInterval(sendShut, 1000);
 
 io.on('connection', (socket) => {
 
-    io.to(socket.id).emit("status-change", JSON.parse('{"sts":"'+0+'"}'));
+    io.to(socket.id).emit("status-change", JSON.parse('{"sts":"'+0m+'"}'));
 
     socket.on('disconnect', () => {
         console.log(`${socket.id} disconnectted`);
